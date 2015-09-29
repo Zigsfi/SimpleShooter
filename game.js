98ed8311fd8a6d34f7e03c60e8afacd012e17f62
lpezzullo <lydia@pezzullo.com>
2015-09-28 21:54:41 -0400

    deleted old, commented code + seeing what happens when you delete render()

--- game.js
+++ game.js
@@ -10,27 +10,15 @@ function preload () {
 }
 
 function create() {
-    //player = game.add.sprite(50, 50, 'player');
     player = new Player(game, 50, 50);
-
-    // player.anchor.setTo(0.5, 0.5);
-
-    // this.cursors = game.input.keyboard.createCursorKeys();
-
-    // wasd = {
-    //     up: game.input.keyboard.addKey(Phaser.Keyboard.W),
-    //     down: game.input.keyboard.addKey(Phaser.Keyboard.S),
-    //     left: game.input.keyboard.addKey(Phaser.Keyboard.A),
-    //     right: game.input.keyboard.addKey(Phaser.Keyboard.D),
-    // };
 }
 
 function update() {
 
 }
 
-function render() {
-    game.debug.body(player);
-}
+// function render() {
+//     game.debug.body(player);
+// }
 
 
