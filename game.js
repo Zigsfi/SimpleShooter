ce8babce1ce935e7d9d315ae4abd818152ccf984
Brian Rappaport <brian.rappaport@tufts.edu>
2015-10-01 09:17:42 -0400

    Completed Lab 2

--- game.js
+++ game.js
@@ -3,45 +3,38 @@ var boundsX = 800, boundsY = 600;
 var game = new Phaser.Game(boundsX, boundsY, Phaser.AUTO, "game", {preload:preload, update:update, create:create});
 
 var ship;
-var wasd;
+
 function preload () {
     game.load.image('ship', 'ship.png');
     game.load.image('enemy', 'evil.png');
+    game.load.image('rock', 'rock.png');
 }
 
 function create() {
-    ship = game.add.sprite(50, 50, 'ship');
-
-    ship.anchor.setTo(0.5, 0.5);
-    this.cursors = game.input.keyboard.createCursorKeys();
-
-    wasd = {
-        up: game.input.keyboard.addKey(Phaser.Keyboard.W),
-        down: game.input.keyboard.addKey(Phaser.Keyboard.S),
-        left: game.input.keyboard.addKey(Phaser.Keyboard.A),
-        right: game.input.keyboard.addKey(Phaser.Keyboard.D),
-    };
+    ship = new Ship(game, game.world.centerX, game.world.centerY);
+    enemies = game.add.group();
+    rocks = game.add.group();
+    enemies.enableBody = true;
+    rocks.enableBody = true;
+    for (var i = 0;i<4;i++){
+        var enemy = Enemy(enemies, 200 + i*50, 300);
+        var rock = rocks.create(100 + 40*Math.Random(), 200 + 50*Math.Random(), 'rock');
+    }
 }
 
 function update() {
-    var mX = game.input.mousePointer.x;
-    var mY = game.input.mousePointer.y;
-    /* look at the mouse */
-    ship.angle = Math.atan2(ship.position.x - mX, ship.position.y - mY)  * -57.2957795;
-
-    if (wasd.up.isDown) {
-        ship.y -= 3;
-    }
-    if (wasd.down.isDown) {
-        ship.y += 3;
-    }
-    if (wasd.left.isDown) {
-        ship.x -= 3;
-    }
-    if (wasd.right.isDown) {
-        ship.x += 3;
-    }
+    game.physics.arcade.collide(ship, enemies);
+    game.physics.arcade.collide(ship, rocks);
+    game.physics.arcade.overlap(ship, enemies, shipDeath, null, this);
+    game.physics.arcade.overlap(ship, rocks, destroyRock, null, this);
 
 }
 
+function shipDeath(ship, enemy) {
+    ship.kill();
+    game.shutdown();
+}
 
+function destroyRock(ship, rock) {
+    rock.kill();
+}
