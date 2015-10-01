f9630b2daea97225844ddd0f39ea52ea01135a1e
Alena Borisenko <alena.borisenko@gmail.com>
2015-09-30 22:53:43 -0400

    Very basic draft: static spinning enemies that kill the player sprite on contact, randomly placed rocks that are killed when player collides with them.

--- game.js
+++ game.js
@@ -4,16 +4,34 @@ var game = new Phaser.Game(boundsX, boundsY, Phaser.AUTO, "game", {preload:prelo
 
 var ship;
 var wasd;
+
 function preload () {
     game.load.image('ship', 'ship.png');
     game.load.image('enemy', 'evil.png');
+    game.load.image('rock', 'rock.png');
+}
+
+function collisionHandler(player, collider) {
+    collider.collide();
+}
+
+function enemyCollision (player) {
+
+    //  If the player collides with the chillis then they get eaten :)
+    //  The chilli frame ID is 17
+
+    ship.kill();
 }
 
 function create() {
-    ship = game.add.sprite(50, 50, 'ship');
+//    ship = game.add.sprite(50, 50, 'ship');
+////???
+    game.physics.enable(Phaser.Physics.ARCADE);
+    ship = new Player(game, game.world.centerX, game.world.centerY);
 
-    ship.anchor.setTo(0.5, 0.5);
+ //   ship.anchor.setTo(0.5, 0.5);
     this.cursors = game.input.keyboard.createCursorKeys();
+
 
     wasd = {
         up: game.input.keyboard.addKey(Phaser.Keyboard.W),
@@ -21,14 +39,42 @@ function create() {
         left: game.input.keyboard.addKey(Phaser.Keyboard.A),
         right: game.input.keyboard.addKey(Phaser.Keyboard.D),
     };
+
+
+///???
+    enemies = game.add.group();
+    viewGroup = game.add.group();
+    for (var i = 0; i < 10; i ++) {
+        var enemy = Enemy(enemies, 200 + i * 70, 200); 
+    }
+
+    rocks = game.add.group();
+    viewGroup = game.add.group();
+    for (var i = 0; i < 10; i ++) {
+    //    var rock = Rock(rocks, 200 + i * 70, 300); 
+        var rock = Rock(rocks, game.rnd.between(50, boundsX - 50), game.rnd.between(50, boundsY - 50));
+    }
+
 }
 
 function update() {
-    var mX = game.input.mousePointer.x;
-    var mY = game.input.mousePointer.y;
-    /* look at the mouse */
-    ship.angle = Math.atan2(ship.position.x - mX, ship.position.y - mY)  * -57.2957795;
+    enemies.forEach(function(enemy) {
+        enemy.angle++;
+    });
 
+    rocks.forEach(function(rock) {
+        rock.angle += 0.3;
+    });
+
+    game.physics.arcade.overlap(ship, enemies, enemyCollision, null, this);
+    game.physics.arcade.overlap(ship, rocks, collisionHandler, null, this);
+
+//    var mX = game.input.mousePointer.x;
+//    var mY = game.input.mousePointer.y;
+    /* look at the mouse */
+//    ship.angle = Math.atan2(ship.position.x - mX, ship.position.y - mY)  * -57.2957795;
+
+/*
     if (wasd.up.isDown) {
         ship.y -= 3;
     }
@@ -41,6 +87,7 @@ function update() {
     if (wasd.right.isDown) {
         ship.x += 3;
     }
+*/
 
 }
 
