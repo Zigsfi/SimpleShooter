05b20759234307cbde4505b3e9eea0fc5814f66a
Maya Saxena <maya.saxena@tufts.edu>
2015-10-01 12:57:54 -0400

    Refactored lab

--- game.js
+++ game.js
@@ -1,47 +1,53 @@
-
 var boundsX = 800, boundsY = 600;
 var game = new Phaser.Game(boundsX, boundsY, Phaser.AUTO, "game", {preload:preload, update:update, create:create});
 
 var ship;
-var wasd;
+var enemies;
+var numEnemies = 3;
+var enemyMargin = 100;
+
+var rocks;
+var numRocks = 10;
+var rockMargin = 50;
+
 function preload () {
     game.load.image('ship', 'ship.png');
     game.load.image('enemy', 'evil.png');
+    game.load.image('rock', 'rock.png');
 }
 
 function create() {
-    ship = game.add.sprite(50, 50, 'ship');
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    ship = new Ship(game, 50, 50);
 
-    ship.anchor.setTo(0.5, 0.5);
-    this.cursors = game.input.keyboard.createCursorKeys();
+    enemies = game.add.group();
+    for (var i = 0; i < numEnemies; i++) {
+        enemies.add(new Enemy(game, getRandomX(enemyMargin), getRandomY(enemyMargin)));
+    }
 
-    wasd = {
-        up: game.input.keyboard.addKey(Phaser.Keyboard.W),
-        down: game.input.keyboard.addKey(Phaser.Keyboard.S),
-        left: game.input.keyboard.addKey(Phaser.Keyboard.A),
-        right: game.input.keyboard.addKey(Phaser.Keyboard.D),
-    };
+    rocks = game.add.group();
+    for (var i = 0; i < numRocks; i++) {
+        rocks.add(new Rock(game, getRandomX(rockMargin), getRandomY(rockMargin)));
+    }
 }
 
 function update() {
-    var mX = game.input.mousePointer.x;
-    var mY = game.input.mousePointer.y;
-    /* look at the mouse */
-    ship.angle = Math.atan2(ship.position.x - mX, ship.position.y - mY)  * -57.2957795;
-
-    if (wasd.up.isDown) {
-        ship.y -= 3;
-    }
-    if (wasd.down.isDown) {
-        ship.y += 3;
-    }
-    if (wasd.left.isDown) {
-        ship.x -= 3;
-    }
-    if (wasd.right.isDown) {
-        ship.x += 3;
-    }
-
+    game.physics.arcade.overlap(ship, enemies, collideShipWithEnemy);
+    game.physics.arcade.overlap(ship, rocks, collideShipWithRock);
 }
 
+function collideShipWithEnemy() {
+    ship.kill();
+}
 
+function collideShipWithRock(ship, rocks) {
+    rocks.collide();
+}
+
+function getRandomX(margin) {
+    return Math.random() * (boundsX - margin * 2) + margin;
+}
+
+function getRandomY(margin) {
+    return Math.random() * (boundsY - margin * 2) + margin;
+}
