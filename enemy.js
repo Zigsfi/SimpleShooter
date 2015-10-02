8ba4d83bfc11e9ff72b3fe59d774a6a732c96ddf
Caitlin Klein <Cas@Albion.local>
2015-10-01 20:43:07 -0400

    refactored enemy and player code into separate files; added enemy code with collisions

---
+++ enemy.js
@@ -0,0 +1,35 @@
+
+Enemy.prototype = Object.create(Phaser.Sprite.prototype);
+
+Enemy.prototype.constructor = Enemy;
+
+function Enemy(game, x, y) {
+    Phaser.Sprite.call(this, game, x, y, 'enemy');
+    this.dx = Math.floor(Math.random() * 10);
+    this.dy = Math.floor(Math.random() * 10);
+    Enemy.prototype.game = game;
+    this.scale.setTo(0.1, 0.1);
+    this.anchor.setTo(0.5, 0.5);
+    game.add.existing(this);
+    this.collide = function() {
+        this.destroy();
+    };
+    game.physics.enable(this, Phaser.Physics.ARCADE);
+}
+
+Enemy.prototype.update = function() {
+    if ((this.x + this.dx) <= 0) {
+        this.dx = this.dx * -1;
+    }
+    if ((this.x + this.dx) >= this.game.width) {
+        this.dx = this.dx * -1;
+    }
+    if ((this.y + this.dy) <= 0) {
+        this.dy = this.dy * -1;
+    }
+    if ((this.y + this.dy) >= this.game.height) {
+        this.dy = this.dy * -1;
+    }
+    this.x = this.x + this.dx;
+    this.y = this.y + this.dy;
+}
--- game.js
+++ game.js
@@ -1,18 +1,18 @@
-
 var boundsX = 800, boundsY = 600;
 var game = new Phaser.Game(boundsX, boundsY, Phaser.AUTO, "game", {preload:preload, update:update, create:create});
 
 var ship;
 var wasd;
+var enemies;
 function preload () {
     game.load.image('ship', 'ship.png');
     game.load.image('enemy', 'evil.png');
 }
 
 function create() {
-    ship = game.add.sprite(50, 50, 'ship');
+    game.physics.enable(Phaser.Physics.ARCADE);
+    ship = new Ship(game, game.world.centerX, game.world.centerY); 
 
-    ship.anchor.setTo(0.5, 0.5);
     this.cursors = game.input.keyboard.createCursorKeys();
 
     wasd = {
@@ -21,27 +21,20 @@ function create() {
         left: game.input.keyboard.addKey(Phaser.Keyboard.A),
         right: game.input.keyboard.addKey(Phaser.Keyboard.D),
     };
+
+    enemies = game.add.group();
+    for (var i = 0; i < 5; i++) {
+        var en = new Enemy(game, 200 + i * 70, 400);
+        enemies.add(en);
+    }
 }
 
+function collisionHandler(player, collider) {
+    player.kill();
+}
 function update() {
-    var mX = game.input.mousePointer.x;
-    var mY = game.input.mousePointer.y;
     /* look at the mouse */
-    ship.angle = Math.atan2(ship.position.x - mX, ship.position.y - mY)  * -57.2957795;
-
-    if (wasd.up.isDown) {
-        ship.y -= 3;
-    }
-    if (wasd.down.isDown) {
-        ship.y += 3;
-    }
-    if (wasd.left.isDown) {
-        ship.x -= 3;
-    }
-    if (wasd.right.isDown) {
-        ship.x += 3;
-    }
-
+    game.physics.arcade.overlap(ship, enemies, collisionHandler, null, this);
 }
 
 
